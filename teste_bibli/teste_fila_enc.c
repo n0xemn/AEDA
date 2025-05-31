662817c6d8176db39d5c9eccaad19f44d6b3f77d
n0xemn <leonardo.silva4224@gmail.com>
2025-05-31 15:57:46 -0300

    teste

---
+++ teste_bibli/teste_fila_enc.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "./includes/fila_encadeada/minha_fila_encadeada.h"
+
+int main(){
+    fila_enc* F = iniciar();
+    for (int i = 0; i < 5; i++)
+    {
+        inserir(F, i + 1);
+    }
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d ", front(F));
+        remover(F);
+    }
+    F = liberar(F);
+
+    return 0;
+}

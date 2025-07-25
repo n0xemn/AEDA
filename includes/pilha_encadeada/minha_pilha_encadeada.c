0211fc1d6f3274f68cbae97c1564fd3826c063aa
Leonardo <leornado.silva4224@gmail.com>
2025-07-25 08:04:43 -0300

    consertos e ajustes

--- includes/pilha_encadeada/minha_pilha_encadeada.c
+++ includes/pilha_encadeada/minha_pilha_encadeada.c
@@ -13,10 +13,7 @@ struct celula
     celula* prox;
 };
 
-
 static celula* criar_celula(Item it){
-    // pra deixar funcoes fazendo uma coisa especifica. toda vez que precisar criar uma celula nova eh soh chamar essa funcao
-    // e "static" quer dizer que a funcao soh pode ser usada no arquivo em que ela ta.
     celula* nova = (celula*) calloc(1, sizeof(celula));
     if (nova == NULL)
     {
@@ -27,7 +24,7 @@ static celula* criar_celula(Item it){
 }
 
 bool esvaziar(pilha_enc* P_enc){
-    if (P_enc == NULL || P_enc->prim == NULL)
+    if (P_enc == NULL || vazia(P_enc))
     {
         return false;
     }
@@ -40,7 +37,7 @@ bool esvaziar(pilha_enc* P_enc){
 }
 
 pilha_enc* iniciar(){
-    pilha_enc * P_enc = (pilha_enc*) calloc(1, sizeof(pilha_enc)); // mudei pra calloc pra nao te que colocar que qtde e prim recebem 0 ou null 
+    pilha_enc* P_enc = (pilha_enc*) calloc(1, sizeof(pilha_enc)); 
     if (P_enc == NULL)
     {
         return NULL;
@@ -49,13 +46,17 @@ pilha_enc* iniciar(){
 }
 
 bool inserir(pilha_enc* P_enc, Item it){
-    //celula* nova_celula = (celula*) malloc(sizeof(celula)); //mudar
+    if (P_enc == NULL)
+    {
+        return false;
+    }
+    
     celula* nova_celula = criar_celula(it);
     if (nova_celula == NULL)
     {
         return false;
     }
-    //nova_celula->valor = it;
+
     nova_celula->prox = P_enc->prim;
     P_enc->prim = nova_celula;
     return true;
@@ -73,21 +74,23 @@ pilha_enc* liberar(pilha_enc* P_enc){
 }
 
 bool remover(pilha_enc* P_enc){
-    celula* aux = P_enc->prim; // a logica estava errada
-    if (aux != NULL)
+    if (P_enc == NULL)
     {
-        P_enc->prim = aux->prox;
-        free(aux);
-        return true;
+        return false;
     }
-    return false;
+    
+    celula* aux = P_enc->prim; 
+    P_enc->prim = aux->prox;
+    free(aux);
+    return true;
 }
 
 int tamanho(pilha_enc* P_enc){
-    if (P_enc == NULL || P_enc->prim == NULL)
+    if (P_enc == NULL || vazia(P_enc))
     {
         return 0;
     }
+
     celula* cel_aux = P_enc->prim;
     int quant = 1;
     while (cel_aux->prox != NULL)
@@ -95,27 +98,24 @@ int tamanho(pilha_enc* P_enc){
         quant++;
         cel_aux = cel_aux->prox;
     }
+
     return quant;
 }
 
 int top(pilha_enc* P_enc){
-    if (P_enc == NULL)
+    if (P_enc == NULL || vazia(P_enc))
     {
-        printf("Pilha nao iniciada.");
         return 0;
     }
-    if (P_enc->prim == NULL)
-    {
-        printf("Pilha vazia.");
-        return 0;
-    }
+
     return P_enc->prim->valor;
 }
 
 bool vazia(pilha_enc* P_enc){
-    if (P_enc->prim == NULL) // perguntar ele se eu devia dar um exit aqui se nao tiver uma lista iniciada
+    if (P_enc->prim == NULL)
     {
         return true;
     }
+    
     return false;
 }
--- includes/pilha_encadeada/minha_pilha_encadeada.h
+++ includes/pilha_encadeada/minha_pilha_encadeada.h
@@ -9,35 +9,51 @@ typedef struct pilha_enc pilha_enc;
 typedef struct celula celula;
 
 /*
-
+O que faz: Esvazia sua pilha.
+Entrada: Uma pilha.
+Saida: Um valor boleano indicando se deu certo ou nao.
 */
 bool esvaziar(pilha_enc* P_enc);
 /*
-
+O que faz: Inicia a pilha.
+Entrada: Nao tem.
+Saida: Uma pilha iniciada (usar ponteiro).
 */
 pilha_enc* iniciar();
 /*
-
+O que faz: Insere o item no topo da pilha.
+Entrada: Uma pilha e um item.
+Saida: Um valor boleano indicando se deu certo ou nao.
 */
 bool inserir(pilha_enc* P_enc, Item it);
 /*
-
+O que faz: Libera a pilha.
+Entrada: Uma pilha.
+Saida: NULL.
 */
 pilha_enc* liberar(pilha_enc* P_enc);
 /*
-
+O que faz: Remove o item do topo da pilha.
+Entrada: Uma pilha.
+Saida: Um valor boleano indicando se deu certo ou nao.
 */
 bool remover(pilha_enc* P_enc);
 /*
-
+O que faz: Da o tamanho da pilha.
+Entrada: Uma pilha.
+Saida: O tamanho da pilha.
 */
 int tamanho(pilha_enc* P_enc);
 /*
-
+O que faz: Da o elemento do topo da pilha.
+Entrada: Uma pilha.
+Saida: O item que esta no topo da pilha (se a pilha estiver vazia ou se ela nao existir, vai retornar o valor 0).
 */
 int top(pilha_enc* P_enc);
 /*
-
+O que faz: Indica se a pilha esta vazia.
+Entrada: Uma pilha.
+Saida: Um valor boleano indicando se deu certo ou nao.
 */
 bool vazia(pilha_enc* P_enc);
 
